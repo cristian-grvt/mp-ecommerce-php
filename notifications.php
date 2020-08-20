a7a38a3fcbcb0bac433ad641a6f19a582a1440f8
cbartolozzi <cristian@gruvit.com.ar>
2020-08-20 20:47:52 -0300

    1

--- notifications.php
+++ notifications.php
@@ -7,6 +7,7 @@ MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a
 switch($_POST["type"]) {
   case "payment":
       $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+      error_log($payment);
       var_dump($payment);
       die();
       break;
